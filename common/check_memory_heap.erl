d510662f1872006ed43fc07deb62cb66999e2896
Wang Chunye <Wang Chunye>
2016-03-23 09:37:27 +0800

    handle bad match for check_memory_heap.erl

--- common/check_memory_heap.erl
+++ common/check_memory_heap.erl
@@ -1,20 +1,24 @@
 
 Num = 3,
 
-TopMemProcesses = 
+ShowProcess =
+fun({Pid, Memory}) ->
+	{Pid, Memory, process_info(Pid, [registered_name, initial_call, message_queue_len, current_stacktrace])}
+end,
+erlang:memory(),
+TopMemProcesses =
 lists:sublist(
   lists:usort(
     fun({_,A},{_,B}) -> A > B end,
     lists:map(
       fun(Pid) ->
-	      {memory, Memory} = process_info(Pid, memory),
-	      {Pid, Memory}
+	      case process_info(Pid, memory) of
+		  {memory, Memory} ->
+		      {Pid, Memory};
+		  _ ->
+		      {Pid, 0}
+	      end
       end, processes())) , Num),
 
-ShowProcess = 
-fun({Pid, Memory}) ->
-	{Pid, Memory, process_info(Pid, [registered_name, initial_call, message_queue_len, current_stacktrace])}
-end,       
-erlang:memory(),
 lists:map(ShowProcess, TopMemProcesses).
 
