5ceeccf4b809d087b005c9d45690015cd82b7469
WangChunye <wcy123@gmail.com>
2016-04-06 02:05:28 +0800

    update get_group.erl

--- ejabberd/get_group.erl
+++ ejabberd/get_group.erl
@@ -1,26 +1,20 @@
-[User, App, Org] =
-case Args of
-    [_, _, _] -> Args;
-    [_, _] -> [Args] ++ ["easemob-demo"];
-    [_] -> [Args] ++ [ "chatdemoui", "easemob-demo"]
-end,
-AppKey = [Org, "#", App],
+[GroupId] = Args,
 
 Worker = mod_easemob_cache_query_cmd:client(any),
 sys:get_state(Worker),
-eredis:q(Worker, [hgetall, iolist_to_binary(["im:", AppKey, "_" , User])]),
-case eredis:q(Worker, [zrange, iolist_to_binary(["im:", AppKey, "_" , User, ":affiliations"]), 0, -1]) of
+eredis:q(Worker, [hgetall, iolist_to_binary(["im:", GroupId])]),
+case eredis:q(Worker, [zrange, iolist_to_binary(["im:", GroupId, ":affiliations"]), 0, -1]) of
     {ok, Affx} ->
 	io:format("Affiliations: ~p, ~p~n", [erlang:length(Affx), Affx]);
     En ->
 	En
 end,
 
-eredis:q(Worker, [zrange, iolist_to_binary(["im:", AppKey, "_" , User, ":mute"]), 0, -1]),
-eredis:q(Worker, [zrange, iolist_to_binary(["im:", AppKey, "_" , User, ":outcast"]), 0, -1]),
-case muc_mnesia:rpc_get_online_room(iolist_to_binary([AppKey,"_", User]), <<"conference.easemob.com">>) of
+eredis:q(Worker, [zrange, iolist_to_binary(["im:", GroupId, ":mute"]), 0, -1]),
+eredis:q(Worker, [zrange, iolist_to_binary(["im:", GroupId, ":outcast"]), 0, -1]),
+case muc_mnesia:rpc_get_online_room(iolist_to_binary([GroupId]), <<"conference.easemob.com">>) of
     [{_,_,Pid}] ->
-	io:format("~p @ ~p ~p~n", [Pid, node(Pid), mod_muc_room:get(Pid, pq_size)]),
+	io:format("group process ~p @ ~p ~n", [Pid, node(Pid)]),
 	case rpc:call(node(Pid), sys,get_state,[Pid]) of
 	    {_FsmState, State} ->
 		Aff = dict:to_list(element(11,State)),
@@ -30,6 +24,3 @@ case muc_mnesia:rpc_get_online_room(iolist_to_binary([AppKey,"_", User]), <<"con
     _ ->
 	ok
 end.
-
-
-
