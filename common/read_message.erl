d34bafa533a7df70608402a5139d24228a46c86c
Wang Chunye <Wang Chunye>
2016-04-06 20:01:04 +0800

    add read_message.erl

---
+++ common/read_message.erl
@@ -0,0 +1,25 @@
+echo(off),
+[ID] =  Args ,
+
+B = message_store:read(list_to_binary(ID)),
+
+Size = erlang:byte_size(B),
+case Size >= 65530  of
+    true ->
+	io:format("message too long ~p ~p~n", [ID, Size]);
+    false ->
+	case B of
+	    <<"<", _/binary>> ->
+		io:format("XML = ~s~n", [B]);
+	    _ ->
+		ok
+	end,
+	io:format("binary ~p~n", [B]),
+	Meta = msync_msg:decode_meta(B),
+	io:format("Meta ~p~n", [Meta]),
+	msync_msg:encode_meta(Meta)
+end,
+ok.
+
+
+
