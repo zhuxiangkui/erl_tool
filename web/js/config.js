d426e47be6d24e3b67f10d0b45c373a9fddc4248
Yi Letian <letian0805@sina.com>
2016-04-17 11:35:17 +0800

    add web pages

---
+++ web/js/config.js
@@ -0,0 +1,18 @@
+var ToolboxConfig = {
+	disc:"后台查询工具",
+	tools:{
+		muclog:{
+			page: "muclog.html",
+			desc: "MUC日志查询",
+			active: true
+		},
+		msglog:{
+			page: "msglog.html",
+			desc: "消息记录查询"
+		},
+		roster:{
+			page: "empty.html",
+			desc: "好友数量查询"
+		}
+	}
+}
