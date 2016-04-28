db4d01f2f4a99f09d5c11d1c00b6a5825fd18805
Yi Letian <letian0805@sina.com>
2016-04-28 17:08:10 +0800

    make webpage config better

--- web/js/config.js
+++ web/js/config.js
@@ -2,15 +2,18 @@ var ToolboxConfig = {
 	disc:"后台查询工具",
 	tools:{
 		muclog:{
+			host: "http://d.easemob.com:3010",
 			page: "muclog.html",
 			desc: "MUC日志查询",
 			active: true
 		},
 		msglog:{
+			host: "http://182.92.239.119:8000/cgi-bin",
 			page: "msglog.html",
 			desc: "消息记录查询"
 		},
 		roster:{
+			host: "",
 			page: "empty.html",
 			desc: "好友数量查询"
 		}
