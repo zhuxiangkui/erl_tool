dffffc131a38a2dbc7373cb828128a174f52793e
console-easemob <console@easemob.com>
2016-05-16 15:58:13 +0800

    add name of odbc server

--- common/ping_odbc_shards.erl
+++ common/ping_odbc_shards.erl
@@ -1,11 +1,18 @@
 echo(off),
 
-GetDelay =
+GetDelayInfo =
 fun(N) ->
         WorkerName = list_to_atom("odbc_shards_" ++ integer_to_list(N)),
         Worker = cuesport:get_worker(WorkerName),
         SQL = {sql_query, [<<"select 1;">>]},
-        try timer:tc(gen_fsm,sync_send_event,[Worker, {sql_cmd, SQL, os:timestamp()}, 5000]) of
+	State = sys:get_state(Worker),
+%% # {state,<7121.9910.3894>,mysql,
+%% #                             [<<"rds4077hl6n7cv79795i.mysql.rds.aliyuncs.com">>,
+%% #                              3306,<<"ejabberd">>,<<"ejabberd">>,
+%% #                              <<"ejabberd">>],
+%% #                             30000000,1000,
+%% #                             {0,{[],[]}}}
+        Delay = try timer:tc(gen_fsm,sync_send_event,[Worker, {sql_cmd, SQL, os:timestamp()}, 5000]) of
             {Time, {selected,[<<"1">>],[[<<"1">>]]}} ->
                 Time/1000;
             X ->
@@ -15,14 +22,15 @@ fun(N) ->
             C:E ->
                 %% io:format("error ~w~n",[{C,E}]),
                 60002
-        end
+        end,
+	{N, Delay, State}
 end,
 
-Delays = lists:map(GetDelay, lists:seq(0,31)),
+DelayInfos = lists:map(GetDelayInfo, lists:seq(0,31)),
 lists:foreach(
-  fun(Delay) ->
-          io:format("odbc delay ~p~n", [Delay])
-  end, Delays),
+  fun({N, Delay, {_, State}}) ->
+          io:format("odbc delay ~p ~p ~s ~p~n", [N, Delay, lists:nth(1,element(4,State)), lists:nth(2,element(4,State)) ])
+  end, DelayInfos),
 
 ok.
 
