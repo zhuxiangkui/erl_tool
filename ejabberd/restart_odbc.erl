af379e46e6a9218e74e6bc745cced4f34867cd71
Wang Chunye <Wang Chunye>
2016-05-16 17:27:49 +0800

    add new fuction of restart odbc conections

---
+++ ejabberd/restart_odbc.erl
@@ -0,0 +1,4 @@
+echo(on),
+F1 = fun( P ) -> {_,V} = process_info(P, dictionary), {state,PP,_,_,_,_,_,_,_} = proplists:get_value(ejabberd_odbc_state, V), PP end, PL = [F1( P) || P <- ejabberd_odbc_sup:get_pids(<<"easemob.com">>)],
+F2 = fun( P) -> case  erlang:process_info(P, current_function) of {current_function,{p1_mysql_conn,_,_}} -> true; _ -> false end end, PL2 = [ P || P <-erlang:processes(), F2( P) ],
+[ P ! stop || P <- PL2 -- PL].
