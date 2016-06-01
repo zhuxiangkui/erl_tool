4103918a34f7546ffe407b3473b0fdbf9b467442
Wang Chunye <Wang Chunye>
2016-06-01 18:37:14 +0800

    update get_auth_pool_status for debugging

--- msync/get_auth_pool_status.erl
+++ msync/get_auth_pool_status.erl
@@ -1,3 +1,4 @@
+echo(off),
 {ok, PropOld} = application:get_env(msync, user),
 
 PoolSize = proplists:get_value(pool_size, PropOld),
@@ -8,11 +9,12 @@ fun(Integer) ->
         list_to_atom(lists:flatten(io_lib:format("~p_~p", [msync_user,Integer])))
 end,
 
-A = lists:map(
+lists:foreach(
       fun(N) ->
-              {state, Servers, Workers, LstConnectTime}  = sys:get_state(ProcName(N))
+	      Name = ProcName(N),
+              {state, Servers, Workers, LstConnectTime}  = sys:get_state(Name),
+	      io:format("~p workers in ~s~n", [ queue:len(Workers), Name ])
       end, lists:seq(0, PoolSize -1)),
 
-io:format("~p~n", [A]),
 
 ok.
