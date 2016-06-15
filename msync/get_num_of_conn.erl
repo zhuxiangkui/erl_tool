8636d52bf2d2f1f053e7188193ee98acb3aa7797
Wang Chunye <Wang Chunye>
2016-06-15 14:29:18 +0800

    update get_workers

--- msync/get_num_of_conn.erl
+++ msync/get_num_of_conn.erl
@@ -1,2 +1,3 @@
-io:format("~p connections~n",[ets:info(msync_c2s_tbl_sockets, size)]),
+echo(off),
+io:format("~p: ~p connections~n",[node(),ets:info(msync_c2s_tbl_sockets, size)]),
 ok.
--- msync/get_num_of_workers.erl
+++ msync/get_num_of_workers.erl
@@ -1,3 +1,4 @@
-echo(on),
-msync_c2s_guard:get_num_of_workers().
+echo(off),
+io:format("~p ~p ~p~n",[node(),msync_c2s_guard:get_num_of_workers(), ets:info(msync_c2s_tbl_sockets, size)]),
+ok.
 
