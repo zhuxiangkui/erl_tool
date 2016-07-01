7f0685984338b88f327a0df2f78f92440f7c5c7a
WangChunye <wcy123@gmail.com>
2016-07-01 18:19:18 +0800

    add get_session

---
+++ msync/get_session.erl
@@ -0,0 +1,15 @@
+echo(on),
+[JID0] = Args,
+
+
+JID = msync_msg:parse_jid(iolist_to_binary([JID0, "@easemob.com/mobile"])),
+case  msync_c2s_lib:get_pb_jid_prop(JID,socket) of
+    {ok, Socket} ->
+        %% ets:lookup(msync_c2s_tbl_sockets, Socket),
+
+        %%io:format("Socket = ~p~n", [ Socket ]),
+        io:format("JID = ~p~n", [ (catch msync_c2s_lib:get_socket_prop(Socket, pb_jid))]);
+    Other ->
+        io:format(" not found? ~p~n",[Other])
+end,
+ok.
