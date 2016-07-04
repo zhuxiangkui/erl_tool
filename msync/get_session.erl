dfe11fb64915885a4266ec5a2ff59ad8d6abee17
WangChunye <wcy123@gmail.com>
2016-07-04 11:49:07 +0800

    update msync/get_session

--- msync/get_session.erl
+++ msync/get_session.erl
@@ -8,7 +8,8 @@ case  msync_c2s_lib:get_pb_jid_prop(JID,socket) of
         %% ets:lookup(msync_c2s_tbl_sockets, Socket),
 
         %%io:format("Socket = ~p~n", [ Socket ]),
-        io:format("JID = ~p~n", [ (catch msync_c2s_lib:get_socket_prop(Socket, pb_jid))]);
+        io:format("JID = ~p~n", [ (catch msync_c2s_lib:get_socket_prop(Socket, pb_jid))]),
+        io:format("Version = ~p~n", [ (catch msync_c2s_lib:get_socket_prop(Socket, version))]);
     Other ->
         io:format(" not found? ~p~n",[Other])
 end,
