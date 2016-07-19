4722148f0cb3f73c19ffe841ea270efa93e261ac
zhuxiangkui <zhuxiangkui@163.com>
2016-07-19 16:20:25 +0800

    delete msg according to special string

---
+++ common/delete_special_msg_io.erl
@@ -0,0 +1,46 @@
+echo(off),
+RunMode = Args,
+
+DoDelete =
+    fun(To) ->
+        io:format("To: ~p~n", [To]),
+        CidMidList = message_store:get_messages_index(To, undefined),
+        io:format("CidMidList: ~p~n", [CidMidList]),
+        FilterStr1 = <<"Congratulations!!!\n\nMusical.ly reaches 100 million users. \n\nWe would like to express our sincere thanks and appreciation to our small and efficient team but also to everyone who supported us all this years, especially YOU!">>,
+        FilterStr2 = <<"We are sorry to inform you that your last verification attempt was unsuccessful.\n\nPlease retry by visiting www.MuserVoice.com and resubmitting your application.">>,
+        lists:foreach(
+            fun({Cid, Mid}) ->
+                Body = message_store:read_message(Mid),
+                %io:format("Body: ~p~n", [Body]),
+                case binary:match(Body, [FilterStr1, FilterStr2]) of
+                    nomatch ->
+                        ignore;
+                    _ ->
+                        io:format("[Deleted]: Mid: ~p, Body: ~p~n", [Mid, Body]),
+                        case RunMode of
+                            "delete" ->
+                                message_store:delete_message(To, undefined, Cid, Mid),
+                                easemob_message_body:delete_message(Mid);
+                            "dry_run" ->
+                                ignore;
+                            _ ->
+                                ignore
+                        end
+                end
+            end, CidMidList)
+    end,
+
+LoopDelete =
+    fun ReadStdIO() ->
+        case io:get_line('') of
+            eof ->
+                ignore;
+            {error, _} ->
+                ReadStdIO();
+            To0 ->
+                DoDelete(list_to_binary(string:strip(To0, both, $\n))),
+                ReadStdIO()
+        end
+    end,
+
+LoopDelete().
