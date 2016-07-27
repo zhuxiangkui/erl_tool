fea3b4cf1080ef077bb4fbb5eb3fe54ff8349e05
zhangchaoeasemob <zhangchao@easemob.com>
2016-07-27 21:25:30 +0800

    add tool for getting group members

---
+++ common/get_group_members.erl
@@ -0,0 +1,8 @@
+echo(on),
+io:format("Input Muc Room like easemob-demo#chatdemoui_group1~n", []),
+[MucList] = Args,
+Muc = list_to_binary(MucList),
+MembersInCache = mod_muc_admin:get_room_affiliations(Muc, <<"conference.easemob.com">>),
+io:format("members in cache:~p ~n Members num :~p ~n", [MembersInCache, erlang:length(MembersInCache)]),
+MembersInDB = mod_easemob_cache:get_group_affiliations(<<"easemob.com">>, Muc),
+io:format("members in DB:~p ~n Members num :~p ~n", [MembersInDB, erlang:length(MembersInDB)]).
