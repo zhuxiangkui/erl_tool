03a6b8d9b89925c9ba504d806bccb906f5ea8b23
console-easemob <console@easemob.com>
2016-07-28 15:16:04 +0800

    add get_sendmsg_status.erl

---
+++ common/get_sendmsg_status.erl
@@ -0,0 +1,31 @@
+%%--------------------------------------------------------------------
+%% @doc common/get_sendmsg_status.erl
+%%
+%% @spec input: none
+%%
+%% @spec example: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd4 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/get_sendmsg_status.erl
+%%
+%% @spec op:
+%%
+%% @spec result:
+%%
+%% @end
+%%--------------------------------------------------------------------
+
+echo(off),
+
+{ChatQL, GroupChatQL} = mod_easemob_sendmsg:status(<<"easemob.com">>),
+
+lists:foreach(
+    fun(ChatQueue) ->
+        State = sys:get_state(ChatQueue),
+        {state, _Atom, _Easemob, _Redis, _Port, _, QName, {maxrate, Max, Cur, _TimeStamp}, _UpLimit} = State,
+        io:format("~p, ~p, ~p~n", [binary_to_list(QName), Max, trunc(Cur)])
+    end, ChatQL),
+
+lists:foreach(
+    fun(GroupChatQueue) ->
+        State = sys:get_state(GroupChatQueue),
+        {state, _Atom, _Easemob, _Redis, _Port, _, QName, {maxrate, Max, Cur, _TimeStamp}, _UpLimit} = State,
+        io:format("~p, ~p, ~p~n", [binary_to_list(QName), Max, trunc(Cur)])
+    end, GroupChatQL).
