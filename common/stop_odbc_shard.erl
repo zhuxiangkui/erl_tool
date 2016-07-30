177aa447f97f07a7db52610ee40835bdee19769c
console-easemob <console@easemob.com>
2016-07-30 19:05:56 +0800

    add erl script

--- common/stop_odbc_shard.erl
+++ common/stop_odbc_shard.erl
@@ -5,12 +5,12 @@
 %
 % e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/stop_odbc_shard.erl
 
-echo(off),
+echo(on),
 config_odbc_shards:stop_shard(),
 application:set_env(message_store, odbc_shard_conn_switch, false),
 application:set_env(message_store, enable_mysql_index, false),
 application:set_env(message_store, enable_mysql_body, false),
 application:set_env(message_store, enable_ssdb_body, true),
-application:set_env(message_store, message_body_database_type, ssdb).
+% application:set_env(message_store, message_body_database_type, ssdb).
 ok.
 
