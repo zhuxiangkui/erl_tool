812d11b00c6ef1fa17105e9c7100e4870d339533
Wang Chunye <wcy123@gmail.com>
2016-08-02 20:36:04 +0800

    add ejabberd/polling_offling_status.erl

---
+++ ejabberd/polling_offling_status.erl
@@ -0,0 +1,15 @@
+%% input: none
+%%
+%% op: show the current status of whether polling offline is enabled or not
+%%
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd4 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/enable_polling_offline.erl
+
+echo(off),
+
+
+case ejabberd_config:get_option(polling_offline, fun(A) -> A end, false) of
+    true ->
+        io:format("polling_offline is enabled~n");
+    false ->
+        io:format("polling_offline is disabled~n")
+end.
