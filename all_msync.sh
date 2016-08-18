bc03991d66baff01c7f1f44d84567429ebcfe48b
console-easemob <console@easemob.com>
2016-08-18 16:46:20 +0800

    fix bug of all msync

--- all_msync.sh
+++ all_msync.sh
@@ -1,6 +1,6 @@
 for i in `cat  /data/shell/msyncconnlist.txt |sed s/#//g`; do
-    echo '                         '    $i;
-    /data/github_ci/erl_tool/erl_expect -sname msync@${i} -setcookie EASEMOBAAAAAAAAAAEBS $* &
+    echo $i;
+    /data/github_ci/erl_tool/erl_expect -sname msync@${i} -setcookie EASEMOBAAAAAAAAAAEBS $* 
 done
 
 wait
