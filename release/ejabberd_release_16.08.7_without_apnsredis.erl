610bf561e80dd84252af9d80543ecb1ac0c661c6
zhangchaoeasemob <zhangchao@easemob.com>
2016-08-27 16:16:17 +0800

    add release tool for 16.08.7

--- release/ejabberd_release_16.08.7_without_apnsredis.erl
+++ release/ejabberd_release_16.08.7_without_apnsredis.erl
@@ -2,7 +2,7 @@
 %
 % op: release-16.08.7
 %
-% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' release/ejabberd_release_16.08.7.erl
+% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' release/ejabberd_release_16.08.7_without_apnsredis.erl
 
 echo(off),
 config:load_env("/data/apps/opt/ejabberd/etc/ejabberd/message_store.config"),
@@ -10,5 +10,4 @@ UpdateModules = [mod_easemob_cache, health_check, msync2xmpp, mod_offline_shared
 lists:foreach(fun(Module) ->
                       code:purge(Module),
                       code:load_file(Module)
-              end, UpdateModules),
-easemob_redis_pool_sup:connect(apns_mute).
+              end, UpdateModules).
