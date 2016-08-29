bd7545b44cdbd4696dd32a1519562cf499a3a826
zhangchaoeasemob <zhangchao@easemob.com>
2016-08-29 21:16:42 +0800

    add code path

--- common/get_code_path.erl
+++ common/get_code_path.erl
@@ -10,14 +10,14 @@
 % app_config.
 
 echo(off),
-[File, OutFile] = Args,
 NodeName = erlang:atom_to_binary(node(), utf8),
 [_, Domain] = binary:split(NodeName, <<"@">>),
-{ok, ModuleList} = file:consult(File),
-{ok, FD} = file:open(OutFile, [write, append]),
+ModuleList = lists:map(fun(ModuleB) ->
+                               erlang:list_to_atom(ModuleB)
+                       end, Args),
 
 lists:map(fun(Module) ->
                   {_M, _B, Path} = code:get_object_code(Module),
                   B = <<Domain/binary, ":", (list_to_binary(Path))/binary>>,
-                  io:format(FD, "~s~n", [erlang:binary_to_list(B)])
+                  io:format("~s~n", [erlang:binary_to_list(B)])
           end, ModuleList).
