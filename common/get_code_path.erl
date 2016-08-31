f4cb0a4769e4825ee501e4eda8d549faf0930eaa
zhangchaoeasemob <zhangchao@easemob.com>
2016-08-31 16:31:12 +0800

    fix bug of getting code path

--- common/get_code_path.erl
+++ common/get_code_path.erl
@@ -3,20 +3,32 @@
 %
 % op: ejabberd / msync version
 %
-% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/get_code_path.erl /Users/zhangchao/work/github.com/erl_tool/common/a.txt /Users/zhangchao/work/github.com/erl_tool/common/b.txt
+% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/get_code_path.erl a b/c d
 % File Like This
 % msync.
-% msync_c2s_handler.
+% msync_c2s_handler/msync_c2s.
 % app_config.
 
 echo(off),
 NodeName = erlang:atom_to_binary(node(), utf8),
 [_, Domain] = binary:split(NodeName, <<"@">>),
 ModuleList = lists:map(fun(ModuleB) ->
-                               erlang:list_to_atom(ModuleB)
+                               ModuleBinary = erlang:list_to_binary(ModuleB),
+                               case binary:split(ModuleBinary, <<"/">>) of
+                                   [ModuleR, ModuleD] ->
+                                       {erlang:binary_to_atom(ModuleD, utf8), ModuleR};
+                                   _ ->
+                                       erlang:binary_to_atom(ModuleBinary, utf8)
+                               end
                        end, Args),
 
-lists:map(fun(Module) ->
+lists:foreach(fun({Module, ModuleR}) ->
+                  {_M, _B, Path} = code:get_object_code(Module),
+                  PathB = list_to_binary(Path),
+                  PathR = binary:replace(PathB, erlang:atom_to_binary(Module, utf8), ModuleR),
+                  B = <<Domain/binary, ":", PathR/binary>>,
+                  io:format("~s~n", [erlang:binary_to_list(B)]);
+             (Module) ->
                   {_M, _B, Path} = code:get_object_code(Module),
                   B = <<Domain/binary, ":", (list_to_binary(Path))/binary>>,
                   io:format("~s~n", [erlang:binary_to_list(B)])
