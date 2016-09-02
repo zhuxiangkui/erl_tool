c1b552569332d5fca8ec8633a33d60a100063829
zhangchaoeasemob <zhangchao@easemob.com>
2016-09-02 16:25:20 +0800

    fix bug of release-16.08.8

--- release/ejabberd_release_16.08.8.erl
+++ release/ejabberd_release_16.08.8.erl
@@ -18,10 +18,10 @@ application:get_env(ejabberd, is_muc, true) == false,
 
 case IsOnlyConn of
     true ->
-        Md5Expired = [<<"78ae11a8a4b7b5552d42aa7b579b3ba9">>],
+        Md5Expired = [<<"aa9425e6be9296dbd86fb3334ebe2d46">>],
         UpdateModules = [ejabberd_c2s];
     false ->
-        Md5Expired = [<<"78ae11a8a4b7b5552d42aa7b579b3ba9">>, <<"e830c2d8ac9b5ce54b8bc6147bfcc450">>, <<"dacc8cb577de81a69bdc8c200adf5ec9">>, <<"879d96ae66e4186ce80eaf6098975bf9">>, <<"cd3dbbb26acca211447afbad9f21cd4b">>, <<"b8acb962b007bc4c2e1160c1f618ad50">>],
+        Md5Expired = [<<"aa9425e6be9296dbd86fb3334ebe2d46">>, <<"e830c2d8ac9b5ce54b8bc6147bfcc450">>, <<"dacc8cb577de81a69bdc8c200adf5ec9">>, <<"07458865ce8241f17d317b3f9848c870">>, <<"6505327006bf598fb8a8cf2e4624b5a8">>, <<"db2297a52315d6f7f910647da5462332">>],
         UpdateModules = [ejabberd_c2s, app_config, easemob_message_body, ejabberd_sm, mod_message_cache, mod_multi_devices]
 end,
 lists:foreach(fun(Module) ->
@@ -45,5 +45,6 @@ case Md5List == Md5Expired of
     true ->
         io:format("update Ejabberd :~p right, update list:~p ~n", [erlang:node(), UpdateModules]);
     false ->
-        io:format("error !!!!! Ejabberd :~p error, update list:~p ~n", [erlang:node(), UpdateModules])
+        io:format("error !!!!! Ejabberd :~p error, update list:~p ~n", [erlang:node(), UpdateModules]),
+        exit(upgrade_error)
 end.
