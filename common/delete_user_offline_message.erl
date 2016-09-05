887c8f227db0730b57738ddaa2ba207461e24eec
console-easemob <console@easemob.com>
2016-09-05 16:19:29 +0800

    delete offline message with resource

--- common/delete_user_offline_message.erl
+++ common/delete_user_offline_message.erl
@@ -1,5 +1,9 @@
 echo(off),
-[JID] = Args,
 ExpireTime = easemob_resource:get_resource_expire_time(),
-message_store:delete_user(iolist_to_binary([JID, "@easemob.com"]), <<"">>,ExpireTime),
+case Args of
+  [JID] ->
+    message_store:delete_user(iolist_to_binary([JID, "@easemob.com"]), <<"">>, ExpireTime);
+  [JID, Res] ->
+    message_store:delete_user(iolist_to_binary([JID, "@easemob.com"]), list_to_binary(Res), ExpireTime)
+end,
 ok.
