328b141ac0104a6e6da09dfcf85c26e591a87558
zhangchaoeasemob <zhangchao@easemob.com>
2016-09-09 16:20:47 +0800

    fix bug of open apns mute

--- release/ejabberd_open_apns_mute.erl
+++ release/ejabberd_open_apns_mute.erl
@@ -5,5 +5,5 @@
 % e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' release/ejabberd_open_apns_mute.erl
 
 echo(off),
-{ok, _} = config:load_env("/data/apps/opt/ejabberd/etc/ejabberd/message_store.config"),
+ok = config:load_env("/data/apps/opt/ejabberd/etc/ejabberd/message_store.config"),
 easemob_redis_pool_sup:connect(apns_mute).
