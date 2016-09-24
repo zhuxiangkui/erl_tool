53d72f09066966393410df5427bd5c00969b04fa
zhangchaoeasemob <zhangchao@easemob.com>
2016-09-24 15:57:22 +0800

    change echo off

--- ejabberd/kick_users.erl
+++ ejabberd/kick_users.erl
@@ -3,7 +3,7 @@
 %% paras:
 %% ex: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/kick_users.erl
 %%
-echo(on),
+echo(off),
 
 [Pid ! system_shutdown || {_, Pid, _, _} <- supervisor:which_children(whereis('ejabberd_c2s_sup'))],
 
