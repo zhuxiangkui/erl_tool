a5b7982f55a1d3a7e5411f06148969d2e771da70
zhangchaoeasemob <zhangchao@easemob.com>
2016-09-27 09:30:27 +0800

    add result to min message internal

--- ejabberd/load_mod_muc_min_message_internal.erl
+++ ejabberd/load_mod_muc_min_message_internal.erl
@@ -14,4 +14,6 @@ Module = mod_muc,
 Host = <<"easemob.com">>,
 Opts = ejabberd_config:get_option({modules,global},fun(Mods) -> proplists:get_value(Module, Mods) end, []),
 ets:insert(ejabberd_modules, {ejabberd_module, {Module, Host}, Opts}),
+MinMessageInternal = gen_mod:get_module_opt(<<"easemob.com">>, mod_muc, min_message_interval, fun(MMI) when is_number(MMI) -> MMI end, 0),
+io:format("MinMessageInternal:~p ~n", [MinMessageInternal]),
 ok.
