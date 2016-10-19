719eb904314f3d25a18825e62fd2530ec5252f6d
zhangchaoeasemob <zhangchao@easemob.com>
2016-10-19 12:28:39 +0800

    add read roster from odbc

---
+++ common/read_roster_odbc.erl
@@ -0,0 +1,10 @@
+
+% input: JIDFile
+%
+% op: get roster for each JID in file
+%
+% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/read_roster.erl easemob-demo#chatdmeoui_t1
+echo(off),
+[User] = Args,
+Ret = odbc_queries:get_roster(<<"easemob.com">>, User).
+io:format("Ret:~p ~n", [Ret]).
