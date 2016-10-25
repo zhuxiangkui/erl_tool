9035beaca350475ec8185633dbd88791b2f79488
Jing Tang <clanchun@gmail.com>
2016-10-25 17:06:19 +0800

    Tweak script according to lager_event_watcher (inside lager_sup)

--- common/lager_event_watcher.erl
+++ common/lager_event_watcher.erl
@@ -6,7 +6,7 @@
 %% start: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/lager_event_watcher.erl start
 %% stop ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/lager_event_watcher.erl stop
 %% get_state ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/lager_event_watcher.erl get_state
-%% set ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/lager_event_watcher.erl threshold|interval|marks_len|reboot_after N
+%% set ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/lager_event_watcher.erl threshold|interval|max_over_cnt|reboot_after N
 %% e.g.
 %% ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/lager_event_watcher.erl threshold 1000
 
@@ -16,11 +16,11 @@ case Args of
     ["start"] ->
         Threshold = application:get_env(lager, event_watcher_threshold, 1000),
         Interval = application:get_env(lager, event_watcher_interval, 1000),
-        MarksLen = application:get_env(lager, event_watcher_marks_len, 3),
+        MaxOverCnt = application:get_env(lager, event_watcher_max_over_cnt, 3),
         RebootAfter = application:get_env(lager, event_watcher_reboot_after, 5000),
 
         Watcher = {lager_event_watcher, {lager_event_watcher, start_link, 
-                                         [Threshold, Interval, MarksLen, RebootAfter]},
+                                         [Threshold, Interval, MaxOverCnt, RebootAfter]},
                    permanent, 5000, worker, [lager_event_watcher]},
         
         supervisor:terminate_child(lager_sup, lager_event_watcher),
@@ -37,7 +37,7 @@ case Args of
         lager_event_watcher:get_state();
     [Key, Value] when Key == "threshold";
                       Key == "interval"
-                      Key == "marks_len" 
+                      Key == "max_over_cnt" 
                       Key == "reboot_after" ->
         try list_to_integer(Value) of
             N ->
