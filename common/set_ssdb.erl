054e6ddae644ad7a5ec608da558f6d45b4218a16
zhuxiangkui <zhuxiangkui@163.com>
2016-11-10 12:01:28 +0800

    modify set_ssdb.erl due to code change

--- common/set_ssdb.erl
+++ common/set_ssdb.erl
@@ -1,8 +1,8 @@
-% input: write | read | close_write | close_read
-%
-% op: ssdb control
-%
-% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd4 common/set_ssdb.erl write
+%% input: write | read | close_write | close_read
+%%
+%% op: ssdb control
+%%
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd4 common/set_ssdb.erl write
 
 
 echo(off),
@@ -11,20 +11,29 @@ echo(off),
 case OP of
     % double write ssdb / mysql
     "write" ->
-        application:set_env(message_store, enable_ssdb_body, true);
+        application:set_env(message_store, enable_mysql_body_write, true),
+        application:set_env(message_store, enable_mysql_body_read, true),
+        application:set_env(message_store, enable_ssdb_body_write, true),
+        application:set_env(message_store, enable_ssdb_body_read, false);
     % read ssdb only
     % double write ssdb / mysql
     "read" ->
-        application:set_env(message_store, message_body_database_type, ssdb),
-        application:set_env(message_store, enable_ssdb_body, true);
+        application:set_env(message_store, enable_mysql_body_write, true),
+        application:set_env(message_store, enable_mysql_body_read, false),
+        application:set_env(message_store, enable_ssdb_body_write, true),
+        application:set_env(message_store, enable_ssdb_body_read, true);
     % close write ssdb
     % close read ssdb
     "close_write" ->
-        application:set_env(message_store, enable_ssdb_body, false),
-        application:set_env(message_store, message_body_database_type, mysql);
+        application:set_env(message_store, enable_mysql_body_write, true),
+        application:set_env(message_store, enable_mysql_body_read, true),
+        application:set_env(message_store, enable_ssdb_body_write, false),
+        application:set_env(message_store, enable_ssdb_body_read, false);
     % close read ssdb
     % double write ssdb / mysql
     "close_read" ->
-        application:set_env(message_store, message_body_database_type, mysql),
-        application:set_env(message_store, enable_ssdb_body, true)
+        application:set_env(message_store, enable_mysql_body_write, true),
+        application:set_env(message_store, enable_mysql_body_read, true),
+        application:set_env(message_store, enable_ssdb_body_write, true),
+        application:set_env(message_store, enable_ssdb_body_read, false);
 end.
