dc98fe0b07e52f5dad0fab38a6b6c5031a483c7b
zhuxiangkui <zhuxiangkui@163.com>
2016-11-18 12:32:45 +0800

    refine stop odbc

--- common/stop_odbc_shard.erl
+++ common/stop_odbc_shard.erl
@@ -1,16 +1,17 @@
 
-% input: none
-%
-% op: stop odbc, use ssdb instead
-%
-% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/stop_odbc_shard.erl
+%% input: none
+%%
+%% op: stop odbc, use ssdb instead
+%%
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/stop_odbc_shard.erl
 
 echo(on),
 config_odbc_shards:stop_shard(),
 application:set_env(message_store, odbc_shard_conn_switch, false),
 application:set_env(message_store, enable_mysql_index, false),
-application:set_env(message_store, enable_mysql_body, false),
-application:set_env(message_store, enable_ssdb_body, true),
-% application:set_env(message_store, message_body_database_type, ssdb).
+application:set_env(message_store, enable_mysql_body_write, false),
+application:set_env(message_store, enable_mysql_body_read, false),
+application:set_env(message_store, enable_ssdb_body_write, true),
+application:set_env(message_store, enable_ssdb_body_read, true),
 ok.
 
