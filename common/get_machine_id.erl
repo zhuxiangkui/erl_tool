03d24913c59ef152e5a56e2fb91633a0db056857
Wang Chunye <wcy123@gmail.com>
2016-11-26 15:26:20 +0800

    fix can't get version

--- common/get_machine_id.erl
+++ common/get_machine_id.erl
@@ -2,6 +2,6 @@ echo(off),
 PID = erlang:whereis(ticktick_id),
 case sys:get_state(PID) of
   {state, _ ,MachineID, _ , _} ->
-    io:format("machine_id ~p~n", [MachineID]);
+    io:format("node ~p  machine_id ~p~n", [node(), MachineID]);
   _ -> not_found
 end.
--- msync/get_version.erl
+++ msync/get_version.erl
@@ -1,14 +1,16 @@
 echo(off),
 [User] = Args,
 BJid = list_to_binary(User),
-{Version, Ip} = case ejabberd_sm:get_session(BJid, <<"easemob.com">>, <<"mobile">>) of
-            [{session, _, _, _, _, [{ip, {{P1, P2, P3, P4}, Port}}, _,  {_, Sock1}, _]}] ->
-                case msync_c2s_lib:get_socket_prop(Sock1, version) of
-                    {ok, V} ->
-                        IpPort = binary_to_atom(iolist_to_binary(io_lib:format("~p.~p.~p.~p:~p", [P1, P2, P3, P4, Port])), latin1),
-                        {V, IpPort};
-                    _ -> {version_not_found, not_found}
-                end;
+Session = easemob_session:get_session(BJid, <<"easemob.com">>, <<"mobile">>),
+{Version, Ip} = case Session of
+            {ok, {session, _, _, _, _, Info}} ->
+                {{P1, P2, P3, P4}, Port} = proplists:get_value(ip, Info),
+                V = case proplists:get_value(client_version, Info) of
+                        {_Type, {V1, V2, V3}} -> binary_to_list(iolist_to_binary([integer_to_binary(V1), ".",  integer_to_binary(V2), ".", integer_to_binary(V3)]));
+                        _ -> undefined
+                    end,
+                IpPort = binary_to_atom(iolist_to_binary(io_lib:format("~p.~p.~p.~p:~p", [P1, P2, P3, P4, Port])), latin1),
+                {V, IpPort};
             _ -> {session_not_found, not_found}
           end,
 io:format("node: ~p  user: ~p client version: ~p  ip:port ~p~n", [node(), User, Version, Ip]).
