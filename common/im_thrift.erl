838f5a013c1bc56484324cb004ed2127f9a642b3
Jing Tang <clanchun@gmail.com>
2016-12-08 17:50:37 +0800

    Add restart to im_thrift application switch

--- common/im_thrift.erl
+++ common/im_thrift.erl
@@ -1,14 +1,19 @@
 %% input: none
 %%
-%% op: start/stop im_thrift application
+%% op: restart/start/stop im_thrift application
 %%
 %% e.g. 
+%% restart: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/im_thrift.erl restart
 %% start: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/im_thrift.erl start
 %% stop: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/im_thrift.erl stop
 
 echo(off),
 
 case Args of
+    ["restart"] ->
+        application:stop(im_thrift),
+        application:start(im_thrift),
+        ok;
     ["start"] ->
         application:start(im_thrift),
         ok;
