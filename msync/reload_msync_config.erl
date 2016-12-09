474d46ab324f11881e9c878e58a2bf5248dfc379
console-easemob <console@easemob.com>
2016-12-09 15:54:54 +0800

    change echo off

--- msync/reload_msync_config.erl
+++ msync/reload_msync_config.erl
@@ -3,7 +3,6 @@
 %% paras:
 %% ex: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/reload_msync_config.erl
 %%
-echo(on),
 echo(off),
 {value, {_, _, Vsn}} = lists:keysearch(msync, 1, application:which_applications()),
 ConfigFile = "/data/apps/opt/msync/releases/" ++ Vsn ++ "/sys.config",
