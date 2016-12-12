a446e027a0eff5a3caf35be68c86f7b486466269
ZouJinHai <zoujinhai@yeah.net>
2016-12-12 12:47:36 +0800

    add erl_tool for app message limit

---
+++ ejabberd/msglimit_change_app_speed.erl
@@ -0,0 +1,18 @@
+%% input: AppKey Speed
+%%
+%% op: change app muc message send speed
+
+%% Speed: the speed on each msglimit consume node, if there is 2 node , the real limit speed is Speed * 2
+
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_app_speed.erl easemob-demo#chatdemoui 100
+
+
+echo(off),
+case Args of
+    [AppKey, Speed] ->
+        io:format("change_app_speed App:~s,Speed:~s,result:~p~n", 
+                  [AppKey, Speed,
+                   mod_message_limit:change_app_speed(list_to_binary(AppKey), 
+                                               list_to_integer(Speed))])
+end,
+ok.
---
+++ ejabberd/msglimit_status.erl
@@ -0,0 +1,10 @@
+%% input: AppKey Speed
+%%
+%% op: get message limit status
+
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_status.erl
+
+
+echo(off),
+mod_message_limit:status(),
+ok.
