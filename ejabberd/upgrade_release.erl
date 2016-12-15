79ac1843afd2d0bf5d1baa06da84d46e6d3d0aa5
zhangchaoeasemob <zhangchao@easemob.com>
2016-12-15 17:26:36 +0800

    rm ejabberd.yml and message_store.config

--- ejabberd/upgrade_release.erl
+++ ejabberd/upgrade_release.erl
@@ -14,18 +14,10 @@ case Args of
         DefaultPath = "/data/apps/opt"
 end,
 release_handler:unpack_release("ejabberd_" ++ Vsn),
-{ok, _} = file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/" ++ Vsn ++ "/ejabberd.yml",
-                    filename:join([DefaultPath ++ "/ejabberd/releases", Vsn, "ejabberd.yml"])),
-{ok, _} = file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/" ++ Vsn ++ "/ejabberd.yml",
-                    filename:join([DefaultPath ++ "/ejabberd/etc/ejabberd", "ejabberd.yml"])),
 {ok, _} = file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/ejabberdctl.cfg",
                     filename:join([DefaultPath ++ "/ejabberd/releases", Vsn, "ejabberdctl.cfg"])),
-{ok, _} = file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/" ++ Vsn ++ "/message_store.config",
-                    filename:join([DefaultPath ++ "/ejabberd/releases", Vsn, "message_store.config"])),
 file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/" ++ Vsn ++ "/sys.config",
                     filename:join([DefaultPath ++ "/ejabberd/releases", Vsn, "sys.config"])),
-{ok, _} = file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/" ++ Vsn ++ "/message_store.config",
-                    filename:join([DefaultPath ++ "/ejabberd/etc/ejabberd", "message_store.config"])),
 {Ok, _} = file:copy(DefaultPath ++ "/ejabberd/etc/ejabberd/inetrc",
                     filename:join([DefaultPath ++ "/ejabberd/releases", Vsn, "inetrc"])),
 
