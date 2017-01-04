a8e85cfa93aa09f12c12cb26313f64d3ee290d59
zhangchaoeasemob <zhangchao@easemob.com>
2017-01-04 12:14:01 +0800

    add tool for set thrift group

---
+++ common/set_thrift_group.erl
@@ -0,0 +1,23 @@
+%%%
+%% Feature:
+%% paras:
+%% ex: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/set_thrift_group.erl true
+%%
+echo(off),
+Bool =
+case Args of
+    ["true"] ->
+        true;
+    ["false"] ->
+        false;
+    _ ->
+        bad_args
+end,
+case Bool == bad_args of
+    true ->
+        io:format("Error: you should input args like: true or false ~n", []);
+    false ->
+        application:set_env(message_store, enable_thrift_group, Bool),
+        io:format("you have set thrift group :~p ~n", [Bool])
+end,
+ok.
