92308c7aa49e33554eed741d13ee17100c711653
console-easemob <console@easemob.com>
2017-02-21 18:46:36 +0800

    refine commom/read_roster.erl

--- common/read_roster.erl
+++ common/read_roster.erl
@@ -1,11 +1,11 @@
 
-% input: JIDFile
-%
-% op: get roster for each JID in file
-%
-% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/read_roster.erl JIDFile
+%% input: JID
+%%
+%% op: get roster for given JID
+%%
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/read_roster.erl JID
 
-[UserFile] = Args,
-io:format("UserFile~p ~n", [UserFile]),
-{ok, UserList} = file:consult(UserFile),
-lists:foreach(fun(User) -> mod_roster:get_roster(User, <<"easemob.com">>) end, UserList).
+echo(off),
+[JID] = Args,
+%io:format("JID~p ~n", [JID]),
+io:format("rosters: ~p~n", [mod_roster:get_roster(list_to_binary(JID), <<"easemob.com">>)]).
