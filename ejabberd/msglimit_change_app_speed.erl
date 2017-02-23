570bcb11963e852fc8cd94213bc18c334659463e
ZouJinHai <zoujinhai@yeah.net>
2017-02-23 19:47:50 +0800

    add msglimit_change_queue_speed.erl to change kafka queue read speed

--- ejabberd/msglimit_change_app_speed.erl
+++ ejabberd/msglimit_change_app_speed.erl
@@ -5,7 +5,7 @@
 %% Speed: the speed on each msglimit consume node, if there is 2 node , the real limit speed is Speed * 2
 
 %% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_app_speed.erl easemob-demo#chatdemoui 100
-
+%% This tool is not Use after Ejabberd Release 17.1.7, please use ejabberd/msglimit_change_queue_speed.erl
 
 echo(off),
 case Args of
---
+++ ejabberd/msglimit_change_queue_speed.erl
@@ -0,0 +1,21 @@
+%% input: QueueId Speed
+%%
+%% op: change queue read speed
+%% QueueId: integer or string
+
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_queue_speed.erl 1 100
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_queue_speed.erl message_limit_queue_1 100
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_queue_speed.erl message_limit_queue_10 100
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_queue_speed.erl kafka_message_queue_large_chatroom_low_1 100
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_queue_speed.erl kafka_message_queue_large_chatroom_normal_1 100
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/msglimit_change_queue_speed.erl kafka_message_queue_large_group_normal_1 100
+
+echo(off),
+case Args of
+    [QueueId, Speed] ->
+        io:format("change_queue_speed QueueId:~s,Speed:~s,result:~p~n",
+                  [QueueId, Speed,
+                   mod_message_limit:change_queue_speed(list_to_binary(QueueId),
+                                               list_to_integer(Speed))])
+end,
+ok.
