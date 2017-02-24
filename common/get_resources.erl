2164465ca369e25305a6e9a70453e64b4cd0055d
zhangchaoeasemob <zhangchao@easemob.com>
2017-02-24 12:09:08 +0800

    fix bug of get resources

--- common/get_resources.erl
+++ common/get_resources.erl
@@ -3,7 +3,7 @@
 %% paras:
 %% ex: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/get_resources.erl
 %%
-echo(on),
+echo(off),
 [EID] = Args,
-Resources = easemob_resource:get_resources(EID),
+Resources = easemob_resource:get_resources(list_to_binary(EID)),
 io:format("~p's Resources:~p ~n ", [Resources]).
