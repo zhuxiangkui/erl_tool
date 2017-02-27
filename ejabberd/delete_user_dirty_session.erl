3d2a44e7ff8db90fa689c2f6fd76af8099c43bcc
ZouJinHai <zoujinhai@yeah.net>
2017-02-27 17:17:18 +0800

    change desc for delete_user_dirty_session.erl

--- ejabberd/delete_user_dirty_session.erl
+++ ejabberd/delete_user_dirty_session.erl
@@ -1,8 +1,11 @@
 %% input: UserSessionKeyFile
 %% UserSessionKeyFile: the file that contains all the keys of the user session
+
 %% op: delete user dirty session
-%% !!! must copy the UserSessionKeyFile to /data/apps/opt/ejabberd/users-session.data on the target ejabberd node
-%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/delete_user_dirty_session.erl
+
+%% e.g.:  1. Chose a Target ejabberd node
+%%        2. copy the UserSessionKeyFile to /data/apps/opt/ejabberd/users-session.data on the chosed node
+%%        3. ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' ejabberd/delete_user_dirty_session.erl
 
 
 echo(off),
