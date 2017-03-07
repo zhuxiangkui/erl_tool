ba2ebaa7f4129888257cbb13e0b08f4d642f26d0
ZouJinHai <zoujinhai@yeah.net>
2017-03-07 11:07:36 +0800

    add tools: queue muc message into kafka

---
+++ common/set_large_muc.erl
@@ -0,0 +1,15 @@
+
+% input: AppKey
+%
+% op: queue group or chatroom message into kafka
+%
+% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/set_large_muc.erl GroupOrChatroom
+% 
+%
+% GroupOrChatroom: easemob-demo#chatdemoui_1111111
+
+echo(off),
+[GroupOrChatroom] = Args,
+GroupOrChatroomId = <<(list_to_binary(GroupOrChatroom))/binary,"@conference.easemob.com">>,
+io:format("Res:~p~n", [mod_message_limit:set_large_muc(GroupOrChatroomId)]),
+ok.
---
+++ common/unset_large_muc.erl
@@ -0,0 +1,15 @@
+
+% input: AppKey
+%
+% op: do not queue group or chatroom message into kafka
+%
+% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/unset_large_muc.erl GroupOrChatroom
+% 
+%
+% GroupOrChatroom: easemob-demo#chatdemoui_1111111
+
+echo(off),
+[GroupOrChatroom] = Args,
+GroupOrChatroomId = <<(list_to_binary(GroupOrChatroom))/binary,"@conference.easemob.com">>,
+io:format("Res:~p~n", [mod_message_limit:unset_large_muc(GroupOrChatroomId)]),
+ok.
