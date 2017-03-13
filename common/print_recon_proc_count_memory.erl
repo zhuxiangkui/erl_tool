da3ce9fb5edbc25ffea062564f1dbcaf81e32190
taotao.lin <lintt@easemob.com>
2017-03-13 15:09:07 +0800

    add monitor process for ejabberd

--- common/print_recon_proc_count_memory.erl
+++ common/print_recon_proc_count_memory.erl
@@ -1,5 +1,26 @@
 echo(off),
-io:format("~p~n", [[{X/1024/1024, recon:info(P)} || {P, X, _} <-  recon:proc_count(memory, 3)]]),
-io:format("~p~n", [recon_alloc:memory(usage)]),
+io:format("========================================~n"),
+io:format("time : ~p~n", [erlang:localtime()]),
+io:format("---> memory proc count info~n"),
+[begin
+    io:format("~p~n", [[Proc, {'memory(M)', Memory/1024/1024}, recon:info(Proc, registered_name),
+                        recon:info(Proc, current_location), recon:info(Proc, current_function)]])
+ end || {Proc, Memory, _} <- recon:proc_count(memory, 5)],
+io:format("---> reductions proc window info~n"),
+[begin
+    io:format("~p~n", [[Proc, {'reductionwindow', ReductionsWindow}, recon:info(Proc, registered_name),
+                        recon:info(Proc, current_location), recon:info(Proc, current_function)]])
+ end || {Proc, ReductionsWindow, _} <- recon:proc_window(reductions, 5, 500)],
+io:format("---> message_queue_len proc count info~n"),
+[begin
+    io:format("~p~n", [[Proc, {'message_queue_len', MsgQueueLen}, recon:info(Proc, registered_name),
+                        recon:info(Proc, current_location), recon:info(Proc, current_function)]])
+ end || {Proc, MsgQueueLen, _} <- recon:proc_count(message_queue_len, 5)],
+io:format("---> binary leak info~n"),
+[begin
+    io:format("~p~n", [[Proc, {'bin_leak', BinLeak}, recon:info(Proc, registered_name),
+                        recon:info(Proc, current_location), recon:info(Proc, current_function)]])
+ end || {Proc, BinLeak, _} <- recon:bin_leak(5)],
+io:format("---> total erlang process num~n"),
+io:format("~p~n", [erlang:length(erlang:processes())]),
 ok.
-
