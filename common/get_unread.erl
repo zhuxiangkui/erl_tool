4d8325c20f400cf6efb9c71d970a28f9b68c0608
zhangchaoeasemob <zhangchao@easemob.com>
2017-03-16 17:56:59 +0800

    fix bug of get unread

--- common/get_unread.erl
+++ common/get_unread.erl
@@ -7,6 +7,6 @@ echo(off),
 [UserIOList, ResourceIOList] = Args,
 User = list_to_binary(UserIOList),
 Resource = list_to_binary(ResourceIOList),
-Unread = easemob_offline_unread:get_unread(<<User/binary, "@easemob.com", Resource/binary>>),
+Unread = easemob_offline_unread:get_unread(<<User/binary, "@easemob.com/", Resource/binary>>),
 io:format("Unread:~p ~n", [Unread]),
 ok.
