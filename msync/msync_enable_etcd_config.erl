69915c09c8a3cb05e4b9a7faa4839cc81d1cff1b
taotao.lin <lintt@easemob.com>
2017-03-20 17:00:13 +0800

    add switch of etcd config for msync

---
+++ msync/msync_enable_etcd_config.erl
@@ -0,0 +1,27 @@
+echo(off),
+EtcdClient =
+    {msync_etcd_client,
+     {msync_etcd_client, start_link, []},
+     permanent,
+     brutal_kill,
+     worker,
+     [msync_etcd_client]},
+case erlang:whereis(msync_etcd_client) of
+    Pid when erlang:is_pid(Pid) ->
+        ok;
+    _ ->
+        case supervisor:start_child(msync_sup, EtcdClient) of
+            {error, already_present} ->
+                supervisor:restart_child(msync_sup, msync_etcd_client),
+                ok;
+            {error, {already_started, _}} ->
+                ok;
+            {ok, _, _} ->
+                ok;
+            {ok, _} ->
+                ok
+        end
+end,
+true = erlang:is_pid(erlang:whereis(msync_etcd_client)),
+ok = msync_etcd_config:enable_etcd_config(),
+ok.
