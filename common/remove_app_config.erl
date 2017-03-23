a57dbb299ab91cf46055a73d75a19c8164a289bb
Jing Tang <clanchun@gmail.com>
2017-03-23 19:33:29 +0800

    Remove app config

---
+++ common/remove_app_config.erl
@@ -0,0 +1,29 @@
+%% input: AppKey, ConfigName
+%%
+%% op: remove ConfigName for AppKey
+%%
+%% e.g.: ./erl_expect -sname msync@sdb-ali-hangzhou-ejabberd5 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/remove_app_config.erl easemob-demo#chatdemoui check_nickname
+
+echo(off),
+
+case Args of
+    [RawAppKey, RawConfigName] ->
+        AppKey = iolist_to_binary(RawAppKey),
+        ConfigName = list_to_atom(RawConfigName),
+        case app_config:remove_app_config_global(AppKey, ConfigName) of
+            ok ->
+                case app_config:remove_app_config(AppKey, ConfigName) of
+                    {atomic, _} ->
+                        io:format("remove ~s for ~s succeed~n", [ConfigName, AppKey]);
+                    _ ->
+                        io:format("remove ~s for ~s failed~n", [ConfigName, AppKey])
+                end;
+            _ ->
+                io:format("remove ~s for ~s failed~n", [ConfigName, AppKey])
+        end;
+    _ ->
+        io:format("wrong args: ~p~n", [Args])
+end,
+ok.
+        
+    
