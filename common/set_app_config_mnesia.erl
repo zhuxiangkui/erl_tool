2a7452015562567135315a0df6dea0d293344d59
Nassue <jianchensue@163.com>
2017-03-30 17:01:43 +0800

    set app_config just for mnesia

---
+++ common/set_app_config_mnesia.erl
@@ -0,0 +1,9 @@
+%% op: syn appconfig from redis to mnesia
+%%
+%% e.g.:  ./erl_expect -sname msync@sdb-ali-hangzhou-ejabberd5 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/set_app_config_mnesia.erl
+
+echo(off),
+io:format("syn appconfig from redis to mnesia => ~p~n",[app_config:load_app_config()]),
+ok.
+
+
