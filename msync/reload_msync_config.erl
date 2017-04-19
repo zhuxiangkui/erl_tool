f18b8b1f55c410b0a7482a56ee914d3a840f4586
ZouJinHai <zoujinhai@yeah.net>
2017-04-19 17:43:50 +0800

    change reload msync config vsn read

--- msync/reload_msync_config.erl
+++ msync/reload_msync_config.erl
@@ -4,7 +4,8 @@
 %% ex: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/reload_msync_config.erl
 %%
 echo(off),
-{value, {_, _, Vsn}} = lists:keysearch(msync, 1, application:which_applications()),
+%{value, {_, _, Vsn}} = lists:keysearch(msync, 1, application:which_applications()),
+[{_,Vsn,_,_}] = release_handler:which_releases(permanent),
 ConfigFile = "/data/apps/opt/msync/releases/" ++ Vsn ++ "/sys.config",
 config:load_env(ConfigFile),
 io:format("load config finished ~n",[]).
