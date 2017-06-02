ec17345bfc8a022a19325d15822bbf86570066a5
zhuxiangkui <zhuxiangkui@163.com>
2017-06-02 15:49:29 +0800

    do not copy sys.config when reload

--- ejabberd/reload_ejabberd_config.erl
+++ ejabberd/reload_ejabberd_config.erl
@@ -4,10 +4,10 @@
 %
 % e.g.: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/reload_ejabberd_config.erl
 
-echo(off),
+echo(on),
 {value, {_, _, Vsn}} = lists:keysearch(ejabberd, 1, application:which_applications()),
-{ok, _} = file:copy("/data/apps/opt/ejabberd/etc/ejabberd/" ++ Vsn ++ "/sys.config",
-                    filename:join(["/data/apps/opt/ejabberd/etc/ejabberd", "sys.config"])),
+%{ok, _} = file:copy("/data/apps/opt/ejabberd/etc/ejabberd/" ++ Vsn ++ "/sys.config",
+%                    filename:join(["/data/apps/opt/ejabberd/etc/ejabberd", "sys.config"])),
 ConfigFile = "/data/apps/opt/ejabberd/releases/" ++ Vsn ++ "/sys.config",
 config:load_env(ConfigFile),
 ejabberd_config:load_file(ConfigFile),
