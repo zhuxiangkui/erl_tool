61e7b4c8e7fba114e317484017ff1bf585c543ea
ZouJinHai <zoujinhai@yeah.net>
2017-06-05 19:02:12 +0800

    fix restart lager

--- common/restart_lager.erl
+++ common/restart_lager.erl
@@ -5,7 +5,6 @@
 % e.g.: ./erl_expect -sname msync@sdb-ali-hangzhou-ejabberd5 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/restart_lager.erl
 
 echo(on),
-config:load_env("/data/apps/opt/ejabberd/etc/ejabberd/message_store.config"),
 application:stop(lager),
 application:start(lager),
 ok.
