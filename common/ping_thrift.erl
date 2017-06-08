6a1a70aef256c29bc77dd6d276275ee11ccc5372
zhangchaoeasemob <zhangchao@easemob.com>
2017-06-08 12:45:06 +0800

    fix bug of ping thrift

--- common/ping_thrift.erl
+++ common/ping_thrift.erl
@@ -1,57 +1,132 @@
-% input: none
-%
-% op: measure redis visit time
-%
-% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/ping_thrift.erl
+%% input: none
+
+%% op: measure redis visit time
+
+%% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/ping_thrift.erl 
 
 echo(off),
-LogException =
+{ThriftType, LogException} =
 case Args of
-    [] ->
-        false;
-    ["true"] ->
-        true
+    [ThriftTypeList] ->
+        {list_to_atom(ThriftTypeList), false};
+    [ThriftTypeList, LogExceptionList] ->
+        {list_to_atom(ThriftTypeList), list_to_atom(LogExceptionList)}
 end,
 
-PoolSize = extauth_rpc:extauth_opts(<<"easemob.com">>, pool_size),
-Processes =
-lists:map(fun(Index) ->
-                  gen_mod:get_module_proc(iolist_to_binary([<<"easemob.com">>, integer_to_list(Index)]), eauth)
-          end, lists:seq(0,PoolSize - 1)),
-
-UserAuth = {'UserAuth',{'EID',<<"easemob-demo#chatdemoui">>,<<"mt001">>},<<"asd">>,undefined},
-Clients =
-lists:flatmap(fun(P) ->
-                      case catch sys:get_state(whereis(P)) of
-                          {_, _, Workers, _} ->
-                              queue:to_list(Workers);
-                          Exception ->
-                              io:format("Node:~p P:~p Exception:~p ~n", [node(), P, Exception]),
-                              []
-                      end
-              end, Processes),
-Ret =
-lists:filtermap(fun(Client) ->
-                        {_, Res} = (catch thrift_client:call(Client, login, [UserAuth])),
-                        case Res of
-                            {ok, _} ->
-                                false;
-                            Exception ->
-                                case LogException of
-                                    true ->
-                                        io:format("thrift Client:~p  exception:~p ~n", [Client, Exception]);
-                                    false ->
-                                        ignore
-                                end,
-                                {true, Client}
-                        end
-                end, Clients),
-case Ret == [] of
-    true ->
-        io:format("~n Node:~p every client is all right ~p available rate ~p % ~n", [node(), erlang:length(Clients), 100]);
-    false ->
-        FailedLen = erlang:length(Ret),
-        AllLen = erlang:length(Clients),
-        io:format("~n Node:~p Failed ~p All clients is ~p AvailableRate ~p % ~n", [node(), FailedLen, AllLen, (AllLen - FailedLen) / AllLen * 100])
-end,
-ok.
+Block = false,
+Timeout = 1000,
+AppKey = <<"easemob-demo#chatdemoui">>,
+LUser = <<"easemob-demo#chatdemoui_mt001">>,
+To = <<"easemob-demo#chatdemoui_mt002">>,
+IP = {127,0,0,1},
+case ThriftType of
+    auth ->
+        UserAuth = {'UserAuth',{'EID',<<"easemob-demo#chatdemoui">>,<<"mt001">>},<<"asd">>,undefined},
+        case im_thrift:call(user_service_thrift, login, [UserAuth], Block, Timeout) of
+            {ok, _} ->
+                ok;
+            {exception, _} ->
+                ok;
+            Exception ->
+                case LogException of
+                    true ->
+                        io:format("Auth thrift exception:~p ~n", [Exception]),
+                        exit(1);
+                    false ->
+                        ok
+                end
+        end;
+    group ->
+        UID = <<"mt001">>,
+        GID = <<"">>,
+        Params = easemob_muc_opt:make_params(AppKey, UID, group, GID),
+        case im_thrift:call(groupService_thrift, getUserJoinedGroupList, [Params, 1, 20000, UID], Block, Timeout) of
+            {ok, _} ->
+                ok;
+            {exception, _} ->
+                ok;
+            Exception ->
+                case LogException of
+                    true ->
+                        io:format("Group thrift exception:~p ~n", [Exception]),
+                        exit(1);
+                    false ->
+                        ok
+                end
+        end;
+    antispam ->
+        Message = <<"this is antispam test body">>,
+        ChatType = <<"chat">>,
+        Timestamp = jlib:timestamp_to_long(os:timestamp()),
+        IP1 = erlang:list_to_binary(inet_parse:ntoa(IP)),
+        Data = jsx:encode([{app, AppKey}, {sender, LUser}, {acceptor, To}, {time, Timestamp}, {content, Message}, {type, ChatType}, {ip, IP1}]),
+        case im_thrift:call(behavior_service_thrift, getBehaviorSpamProb, [Data], Block, Timeout) of
+            {ok, _} ->
+                ok;
+            {exception, _} ->
+                ok;
+            Exception ->
+                case LogException of
+                    true ->
+                        io:format("Antispam thrift exception:~p ~n", [Exception]),
+                        exit(1);
+                    false ->
+                        ok
+                end
+        end;
+    keyword ->
+        Message = <<"this is keyword test body">>,
+        case im_thrift:call(text_parse_service_thrift, parseWords, [AppKey, Message], Block, Timeout) of
+            {ok, _} ->
+                ok;
+            {exception, _} ->
+                ok;
+            Exception ->
+                case LogException of
+                    true ->
+                        io:format("Keyword thrift exception:~p ~n", [Exception]),
+                        exit(1);
+                    false ->
+                        ok
+                end
+        end;
+    conference ->
+        case im_thrift:call(conference_service_thrift, join_p2p_voice, [LUser, inet_parse:ntoa(IP), To, inet_parse:ntoa(IP)], Block, Timeout) of
+            {ok, _} ->
+                ok;
+            {exception, _} ->
+                ok;
+            Exception ->
+                case LogException of
+                    true ->
+                        io:format("Conference thrift exception:~p ~n", [Exception]),
+                        exit(1);
+                    false ->
+                        ok
+                end
+        end;
+    rtc ->
+        XMLB = <<"<iq id='CONFR__2f7729ccfc' from='easemob-demo#chatdemoui_yss001@easemob.com/webim' to='easemob.com' type='get'><query><MediaReqExt><rtflag>1</rtflag><rtkey/><sid>CONFR__2f7729ccfc</sid><content>{&quot;op&quot;:0, &quot;video&quot;:1, &quot;audio&quot;:1, &quot;peer&quot;:&quot;yss002&quot;, &quot;tsxId&quot;:&quot;WEBIM_2f7729ccfc&quot;}</content></MediaReqExt></query></iq>">>,
+        XML = xml_stream:parse_element(XMLB),
+        Meta = msync_meta_converter:from_xml(XML),
+        RtcFrom = jlib:string_to_jid(<<"easemob-demo#chatdemoui_yss001@easemob.com/webim">>),
+        RtcTo = jlib:make_jid(<<>>, <<"easemob.com">>, <<>>),
+        Header = jsx:encode([{from, jlib:jid_to_string(RtcFrom)},
+                             {to, jlib:jid_to_string(RtcTo)},
+                             {op, ulmsg}]),
+        Binary = msync_msg:encode_meta(Meta),
+        case im_thrift:call(rtc_service_thrift, mediaRequest, [Header, Binary], Block, Timeout) of
+            {ok, _} ->
+                ok;
+            {exception, _} ->
+                ok;
+            Exception ->
+                case LogException of
+                    true ->
+                        io:format("RTC thrift exception:~p ~n", [Exception]),
+                        exit(1);
+                    false ->
+                        ok
+                end
+        end
+end.
