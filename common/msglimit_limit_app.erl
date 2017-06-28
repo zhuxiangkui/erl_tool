f537463b762e3bec6d466f6a37a938d5458be137
ZouJinHai <zoujinhai@yeah.net>
2017-06-28 16:53:44 +0800

    msglimit_limit_app Speed and QueueId can use default value

--- common/msglimit_limit_app.erl
+++ common/msglimit_limit_app.erl
@@ -1,11 +1,13 @@
 % input: AppKey Speed QueueId
+% input: AppKey
 %
 % op: limit app muc message send speed
 %
-% Speed: the speed on each msglimit consume node, if there is 2 node , the real limit speed is Speed * 2
+% Speed: the speed on each worker node, if there is N node , the real limit speed is Speed * min(N,50)
 % QueueId: limit App to which queue. Start from 1 to queue_num, now queue_num=10 
 %
 % e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/msglimit_limit_app.erl easemob-demo#chatdemoui 100 1
+% e.g.: ./erl_expect -sname ejabberd@sdb-ali-hangzhou-ejabberd3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' common/msglimit_limit_app.erl easemob-demo#chatdemoui
 
 echo(off),
 case Args of
@@ -14,6 +16,14 @@ case Args of
                   [AppKey, Speed, QueueId, 
                    mod_message_limit:limit_app(list_to_binary(AppKey), 
                                                list_to_integer(Speed), 
+                                               list_to_integer(QueueId))]);
+    [AppKey] ->
+        Speed = 1,
+        QueueId = 1,
+        io:format("limit App:~s,Speed:~s,QueueId:~s,result:~p~n",
+                  [AppKey, Speed, QueueId,
+                   mod_message_limit:limit_app(list_to_binary(AppKey),
+                                               list_to_integer(Speed),
                                                list_to_integer(QueueId))])
 end,
 ok.
