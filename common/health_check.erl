7a94c60fd467595efdfbeb1ed3f9b9f69be0b33f
zhuxiangkui <zhuxiangkui@163.com>
2017-06-28 20:31:51 +0800

    echo off

--- common/health_check.erl
+++ common/health_check.erl
@@ -3,17 +3,21 @@
 %% paras:
 %% ex: ./erl_expect -sname ejabberd@ebs-ali-beijing-5-pri -setcookie secret $ERL_TOOL_PATH/health_check.erl
 %%
-echo(on),
+echo(off),
 Result = health_check:check(),
-maps:fold(fun(K, V, Acc) ->
-                  #{status := Status} = V,
-                  case Status of
-                      normal->
-                          ok;
-                      warn ->
-                          ok;
-                      false ->
-                          io:format("Result:~p ~n", [Result]),
-                          exit(1)
-                  end
-          end, ok, Result).
+%maps:fold(fun(K, V, Acc) ->
+%                  #{status := Status} = V,
+%                  case Status of
+%                      normal->
+%                          ok;
+%                      warn ->
+%                          ok;
+%                      false ->
+%                          io:format("Result:~p ~n", [Result]),
+%                          exit(1)
+%                  end
+%          end, ok, Result).
+
+io:format("Result:~p ~n", [Result]).
+
+
