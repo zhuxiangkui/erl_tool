a193efe6342aec780cc463330979aa42f391518a
Jing Tang <clanchun@gmail.com>
2017-07-12 12:57:20 +0800

    Rewrite delete gmsg

--- common/delete_gmsg.erl
+++ common/delete_gmsg.erl
@@ -1,59 +1,99 @@
-% input: Num
+% input: appkey or appkey file
 %
 % op: delete gmsg index
 %
-% e.g.: ./erl_expect -sname msync@sdb-ali-hangzhou-msync3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' msync/delete_gmsg.erl 100
+% e.g.: ./erl_expect -sname msync@sdb-ali-hangzhou-msync3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' msync/delete_gmsg.erl -afile absolute/path/to/file
+% e.g.: ./erl_expect -sname msync@sdb-ali-hangzhou-msync3 -setcookie 'LTBEXKHWOCIRRSEUNSYS' msync/delete_gmsg.erl -appkey easemob-demo#chatdemoui
 
 echo(off),
 
 Delete =
-fun (Keys) ->
-        io:format("ks: ~p~n", [Keys]),
-        Qs =
-            lists:foldl(fun (Key, Acc) ->
-                                case binary:match(Key, <<"im:gmsg:cursor">>) of
-                                    nomatch ->
-                                        [[del, Key] | Acc];
-                                    _ ->
-                                        Acc
-                                end
-                        end, [], Keys),
-        io:format("qs: ~p~n", [Qs]),
-        easemob_redis:qp(index, Qs)
-end,
-
-Scan =
-fun S(Cursor, Num) ->
-        case Num =< 0 of
-            true ->
-                io:format("deletion finished~n"),
+fun (AppKey) ->
+        case AppKey of
+            <<>> ->
                 ok;
-            false ->
-                Size = case Num < 100 of
-                           true ->
-                               Num;
-                           false ->
-                               100
-                       end,
-                Q = [scan, Cursor, match, <<"im:gmsg:*">>, count, Size],
-                case easemob_redis:q(index, Q) of
-                    {ok, [<<"0">>, Keys]} ->
-                        Delete(Keys),
-                        io:format("deletion finished~n");
-                    {ok, [NextCursor, Keys]} ->
-                        Delete(Keys),
-                        S(NextCursor, Num - length(Keys));
+            _ ->
+                io:format("delete for appkey: ~p~n", [AppKey]),
+                Q = [[zrange, <<"im:", AppKey/binary, ":groups">>, 0, -1],
+                     [zrange, <<"im:", AppKey/binary, ":chatrooms">>, 0, -1]],
+                case easemob_redis:qp(appconfig, Q) of
+                    [{ok, Groups}, {ok, Chatrooms}] ->
+                        LG = length(Groups),
+                        LC = length(Chatrooms),
+                        Gs = [<<"im:gmsg:", G/binary, "@conference.easemob.com">>
+                                  || G <- Groups ++ Chatrooms],
+                        Qs = [[del, G] || G <- Gs],
+                        case easemob_redis:qp(index, Qs) of
+                            {error, _} ->
+                                io:format("deletion for appkey: ~p failed~n",
+                                          [AppKey]);
+                            Rs ->
+                                L = length([R || {ok, <<"1">>} = R <- Rs]),
+                                io:format("~p in ~p groups or chatrooms deleted"
+                                          " for appkey: ~p~n",
+                                          [L, LG + LC, AppKey])
+                        end;
                     _ ->
-                        ok
+                        io:format("deletion for appkey: ~p failed~n", [AppKey])
                 end
         end
 end,
 
+DoDelete =
+fun (Parent, AppKeys) ->
+        AKs = [binary:replace(A, <<"/">>, <<"#">>) || A <- AppKeys],
+        lists:foreach(fun (A) ->
+                              Delete(A)
+                      end, AKs),
+        Parent ! done
+end,
+
+ParDelete =
+fun PD(Parent, AppKeys) ->
+        case AppKeys of
+            As when length(As) =< 10 ->
+                spawn(fun () -> DoDelete(Parent, As) end);
+            _ ->
+                {Hs, Ts} = lists:split(10, AppKeys),
+                spawn(fun () -> DoDelete(Parent, Hs) end),
+                PD(Parent, Ts)
+        end
+end,
+
+Loop =
+fun L(N) ->
+        case N of
+            0 ->
+                io:format("deletion finished~n");
+            _ ->
+                receive
+                    done ->
+                        L(N - 1)
+                end
+        end
+end,
+
+DeleteFile =
+fun (FileName) ->
+        {ok, Data} = file:read_file(FileName),
+        AppKeys = binary:split(Data, <<"\n">>, [global]),
+        ParDelete(self(), AppKeys),
+        L = length(AppKeys),
+        N = case L rem 10 of
+                0 ->
+                    L div 10;
+                _ ->
+                    L div 10 + 1
+            end,
+        Loop(N)
+end,
         
 case Args of
-    [RawNum] ->
-        Num = list_to_integer(RawNum),
-        Scan(0, Num);
+    ["-appkey", RawAppKey] ->
+        AppKey = list_to_binary(RawAppKey),
+        Delete(AppKey);
+    ["-afile", AppKeyFileName] ->
+        DeleteFile(AppKeyFileName);
     _ ->
         io:format("wrong args~n")
 end,
